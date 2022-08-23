05babeb343943478cb8ec006e6b05fc8d6dcbf9f
sarawutdev <sarawut.w@ach.co.th>
2022-08-23 22:12:20 +0700

    Update index.js

--- server/index.js
+++ server/index.js
@@ -10,7 +10,7 @@ const privateKey = fs.readFileSync(privateKeyPath, "utf8");
 //database
 const syncDatabase = require("./connection/sync-resync");
 const db = require("./models/index.model");
-syncDatabase(true, db);
+syncDatabase(false, db);
 
 app.use(express.json());
 app.use(cors());
